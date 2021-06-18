715743de3a927d0454978cb9aeb7118211ae7eb8
reda-coder <redaelouahabi98@gmail.com>
2021-06-18 01:54:53 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+//   List gogle =[];
+//   var url='https://random-data-api.com/api/omniauth/google_get';
+//  initState(){
+//    super.initialState();
+//    this.fetchData(); }
+
+//  Future fetchData() async{
+//    final http.Response res = await http.get(url);
+//    final data = json.decode(res.body);
+//    setState( () {
+//      this.gogle = data['results'];
+//    });
+
+//  }
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Welcome to Air conseil'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  dynamic data;
+  String data1 = "Hi";
+  void _search() {
+    String url = 'https://api.github.com/';
+    print(url);
+    http.get(Uri.parse(url)).then((response) {
+      setState(() {
+        this.data = json.decode(response.body);
+        data1 = data.toString();
+        print(data1);
+      });
+
+      print(data[0]);
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello World',
+            ),
+            FloatingActionButton(onPressed: () {
+              setState(() {
+                _search();
+              });
+            }),
+            Text(
+              data1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
